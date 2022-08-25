4c981bb3a6a8f3ec8f24f3423470c62bebcd696e
bereberb <bernard.bereber02@gmail.com>
2022-08-25 18:17:49 +0800

    create custom hook for wordle

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ function App() {
     fetch("http://localhost:3001/solutions")
     .then(res => res.json())
     .then(json => {
-      console.log(json)
+      // random number fro 0 -14
+      const randomSolution = json[Math.floor(Math.random()*json.length)]
+      setSolution(randomSolution.word)
     })
-  },[])
+  },[setSolution])
 
 
   return (
     <div className="App">
       <h1>Wordle</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   );
 }
---
+++ src/hooks/useWordle.js
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+const useWordle (solution) => {
+    
+    
+    //format a guess into an array of letter objects
+    //[{key: 'a', color: 'yellow'}]
+    const formatGuess = () => {
+
+    }
+
+    //add a new guess to the guesses state
+    //update the isCorrect state if guess is correct
+    // add one to the turn state
+    const addNewGuess = () => {
+
+    }
+
+    //handle keyup event & track current guess
+    //if user press enter, add new guess
+    const handleKeyup () => {
+
+    }
+    
+}
+
+export default useWordle;
