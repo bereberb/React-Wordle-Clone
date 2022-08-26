ec7e27ed5ae1ada1f123380770d6a9fa84f50f0f
bereberb <bernard.bereber02@gmail.com>
2022-08-26 17:48:29 +0800

    State needed

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -1,7 +1,11 @@
 import { useState } from "react"
 
 const useWordle (solution) => {
-    
+    const [turn, setTurn] = useState(0) 
+    const [currentGuess, setCurrentGuess] = useState('')
+    const [guesses, setGuesses] = useState([]) // each guess is an array
+    const [history, setHistory] = useState([]) // each guess is a string
+    const [isCorrect, setIsCorrect] = useState(false)
     
     //format a guess into an array of letter objects
     //[{key: 'a', color: 'yellow'}]
